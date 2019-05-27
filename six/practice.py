090578229f502750e393e5edd87556425107a6f0
liuzhiqiang123 <275312563@qq.com>
2019-05-27 15:57:04 +0800

    commit

--- six/practice.py
+++ six/practice.py
@@ -89,17 +89,32 @@
 # print("dict1的类型为：",type(dict1))
 
 # 8.
-def rulesort(elem): #定义排序规则
-    return elem['python']
+# 采用自定义函数指定排序规则
+# def rulesort(elem): #定义排序规则
+#     return elem['python']
+#
+# list_dict = [{'name':'无语','python':99,'c':89},
+#              {'name': 'wgh', 'python': 100, 'c': 80},
+#              {'name': '琦琦', 'python': 95, 'c': 97},
+#              {'name': '明日', 'python': 91, 'c': 96},]
+# print("对列表排序前：")
+# for d in list_dict:
+#     print(d)
+# list_dict.sort(key = rulesort,reverse=True) #降序排列
+# print("对列表排序后：")
+# for d in list_dict:
+#     print(d)
 
+#lambda表达式指定排序规则
 list_dict = [{'name':'无语','python':99,'c':89},
              {'name': 'wgh', 'python': 100, 'c': 80},
              {'name': '琦琦', 'python': 95, 'c': 97},
              {'name': '明日', 'python': 91, 'c': 96},]
-print("对列表排序前：")
+print('对列表排序前：')
 for d in list_dict:
     print(d)
-list_dict.sort(key = rulesort,reverse=True) #降序排列
+list_dict.sort(key = lambda x:x['python'],reverse=True)
+
 print("对列表排序后：")
 for d in list_dict:
     print(d)
