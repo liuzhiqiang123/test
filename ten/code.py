a125355f40613e33bdbe7ecd8cf72988ac5a9b2d
liuzhiqiang123 <275312563@qq.com>
2019-06-03 17:51:08 +0800

    commit

---
+++ ten/code.py
@@ -0,0 +1,13 @@
+# 2.
+# name = ['邓肯','吉诺比利','帕克']
+# sign = ['石佛','妖刀','跑车']
+# dictionary = dict(zip(name,sign))
+# print(dictionary)
+
+# 4.
+# name_tuple = ('邓肯','吉诺比利','帕克')
+# sign = ['石佛','妖刀','跑车']
+# dict1 = {name_tuple:sign}
+# print(dict1)
+
+# 6.
