d9bb7a6a8d3bfa221cc3ae64bd986e9a69dbdee3
liuzhiqiang123 <275312563@qq.com>
2019-06-03 16:40:40 +0800

    commit

---
+++ nine/practice.py
@@ -0,0 +1,21 @@
+# 1.并行迭代两个序列
+# s = [1,2,3]
+# t = ['andy','kobe','jay']
+# i = 0
+# z = list()
+# while i < len(s) and i < len(t):
+#     x = s[i]
+#     y = t[i]
+#     z.append((x,y))
+#     i += 1
+# print(z)
+#
+# #
+# s = [1,2,3]
+# t = ['andy','kobe','jay']
+# z = list()
+# for x,y in zip(s,t):
+#     z.append((x,y))
+# print(z)
+
+# 2.
